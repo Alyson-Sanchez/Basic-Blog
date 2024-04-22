1f6a694f2675e04076837998552b36bc96590cc6
Alyson Sanchez <coqui0429@gmail.com>
2024-04-22 18:25:10 -0400

    functioning blog wall & starting blog formatting

--- 02-Challenge/Assets/js/blog.js
+++ 02-Challenge/Assets/js/blog.js
@@ -18,9 +18,23 @@ function loadBlogs(){
     console.log(JSONObject);
     
     paragraphTag.innerHTML = JSONObject.Author +'|'+ JSONObject.Title +'|'+ JSONObject.Body;*/
-    for (let i = 0; i < 100; i++) {
-        createParentDiv();
+    var blogNumbers = localStorage.getItem("indexKey");
+    for (let i = 0; i < blogNumbers; i++) {
+        var newDiv = createParentDiv();
+        fillBlog(newDiv, i);
     }
+
+}
+
+/* TAKES IN PARAMETER OF DIV, AND WILL FILL IN THE INFORMATION */
+
+function fillBlog(newDiv, blogNumber){
+
+    var blogInfo = downloadBlogPost(blogNumber);
+    newDiv.children[2].innerHTML = "Posted by:" + blogInfo.Author;
+    newDiv.children[0].innerHTML = blogInfo.Title;
+    newDiv.children[1].innerHTML = blogInfo.Body;
+
 }
 
 function registerVariables(){
@@ -33,14 +47,17 @@ function createParentDiv(){
    createUserPar(divEl);
    createTitlePar(divEl);
    creatContentPar(divEl);
-
   
-   document.getElementById("exampleDiv").appendChild(divEl); 
+   document.getElementById("exampleDiv").appendChild(divEl);
+   divEl.style.border = "thick solid #000";
+   divEl.style.marginBottom = "10px";
+   return divEl;
 }
 
 function createUserPar(parentDiv){
     var parEl = document.createElement("p");
     parentDiv.appendChild(parEl);
+
 }
 
 function createTitlePar(parentDiv) {
